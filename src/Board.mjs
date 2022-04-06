6741e8be352f3ce1c06d251ab61acbb731ad0396
Ezequiel Omar <ezequiel.omar.vazquez@gmail.com>
2022-04-06 17:08:56 -0300

    feat:toString() on Board class

--- src/Board.mjs
+++ src/Board.mjs
@@ -8,6 +8,16 @@ export class Board {
   }
 
   toString() {
-    return "TODO";
+    if (this.width && this.height) {
+      this.width = parseInt(this.width);
+      this.height = parseInt(this.height);
+      let stringBoard = "";
+      for (let i = 0; i < this.height; i++) {
+        stringBoard += ".".repeat(this.width);
+        stringBoard += "\n";
+      }
+      return stringBoard;
+    }
+    throw new Error("No width/height provided.");
   }
 }
